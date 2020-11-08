08889b49a7d9d4410cdd398aea59cdeaea6b85ba
psytwr <psytwr@exmail.nottingham.ac.uk>
2020-11-08 16:24:40 +0000

    completed insert and started delete

--- RedBlack.hs
+++ RedBlack.hs
@@ -130,17 +130,84 @@ isBlack (NodeRB c l x r) =
 
 
 -- Insert a new element in a RBT, preserving the RBT properties
+--INSERTION INTO A BST
+insertBST :: Ord a => a -> RBT a -> RBT a
+insertBST n LeafRB = (NodeRB Black (LeafRB) n (LeafRB))
+insertBST n (NodeRB c l x r) =
+  if n <= x then NodeRB c (insertBST n l) x r
+            else NodeRB c l x (insertBST n r)
 
+--INSERTION INTO AN RBT
 insertRB :: Ord a => a -> RBT a -> RBT a
-insertRB n LeafRB = (NodeRB Black (LeafRB) n (LeafRB))
-insertRB n (NodeRB c l x r) =
-  if n <= x then NodeRB c (insertRB n l) x r
-            else NodeRB c l x (insertRB n r)
+insertRB n x = blackRoot (ins n x)
 
-            
+ins :: Ord a => a -> RBT a -> RBT a
+ins n LeafRB = (NodeRB Red (LeafRB) n (LeafRB))
+ins n t@(NodeRB c l x r)
+               | n < x = balance c (ins n l) x r
+               | n > x = balance c l x (ins x r)
+               | otherwise = t
+
+blackRoot :: Ord a => RBT a -> RBT a
+blackRoot (NodeRB c l x r) =
+  if c == Black then (NodeRB c l x r)
+                else (NodeRB Black l x r)
+
+redRoot :: Ord a => RBT a -> RBT a
+redRoot (NodeRB c l x r) =
+  if c == Red then (NodeRB c l x r)
+              else (NodeRB Red l x r)
+
+balance :: Color -> RBT a -> a -> RBT a -> RBT a
+balance Black (NodeRB Red (NodeRB Red l x r) x2 r2) x3 r3
+  = NodeRB Red (NodeRB Black l x r) x2 (NodeRB Black r2 x3 r3)  
+balance Black (NodeRB Red l x (NodeRB Red l2 x2 r)) x3 r2
+  = NodeRB Red (NodeRB Black l x l2) x2 (NodeRB Black r x3 r2)
+balance Black l x (NodeRB Red (NodeRB Red l2 x2 r) x3 r2)
+  = NodeRB Red (NodeRB Black l x l2) x2 (NodeRB Black r x3 r2)
+balance Black l x (NodeRB Red l2 x2 (NodeRB Red l3 x3 r))
+  = NodeRB Red (NodeRB Black l x l2) x2 (NodeRB Black l3 x3 r)
+balance c l x r = NodeRB c l x r
+
 
 -- Delete an element from a RBT, preserving the RBT properties
 
---deleteRB :: Ord a => a -> RBT a -> RBT a
---deleteRB n (NodeRB c l x r) = if (searchRB n (NodeRB c l x r)) then --Do a thing
---                                                              else (NodeRB c l x r)
+deleteRB :: Ord a => a -> RBT a -> RBT a
+deleteRB n x = blackRoot (del n x)
+
+del :: Ord a => a -> RBT a -> RBT a
+del n LeafRB = LeafRB
+del n (NodeRB _ l x r)
+             | n < x = delL n l x r
+             | n > x = delR n l x r
+             | otherwise = fuse l r
+
+delL :: Ord a => a -> RBT a -> a -> RBT a -> RBT a
+delL n l x r =
+  if (color l) == Black then balL (del n l) x r
+                        else NodeRB Red (del n l) x r
+
+delR :: Ord a => a -> RBT a -> a -> RBT a -> RBT a
+delR n l x r =
+  if (color r) == Black then balR l x (del n r)
+                        else NodeRB Red l x (del n r)
+
+balL :: Ord a => (RBT a) -> a -> (RBT a) -> (RBT a)
+balL (NodeRB Red l x r) x2 r2 =
+  NodeRB Red (NodeRB Black l x r) x2 r2
+balL l x (NodeRB Black l2 x2 r) =
+  balance Black l x (NodeRB Red l2 x2 r)
+balL l x (NodeRB Red (NodeRB Black l2 x2 r) x3 r2) =
+  NodeRB Red (NodeRB Black l x l2) x2 (balance Black r x3 (redRoot r2))
+
+--balR :: Ord a => RBT a -> a -> RBT a -> RBT a
+
+fuse :: Ord a => RBT a -> RBT a -> RBT a
+fuse (NodeRB Black l x r) (NodeRB Black l2 x2 r2) =
+  let s = fuse r l2
+  in case s of
+    (NodeRB Red s1 z s2) -> (NodeRB Red (NodeRB Black l x s1) z (NodeRB Black s2 x2 r))
+    (NodeRB Black s1 z s2) -> balL (NodeRB Black l x (NodeRB Black s x2 r2))
+
+color :: Ord a => RBT a -> Color
+color (NodeRB c l x r) = c
