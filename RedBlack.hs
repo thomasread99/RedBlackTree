40e7d9033c4cb3618984f66696bff3669993c4b8
psytwr <psytwr@exmail.nottingham.ac.uk>
2020-11-10 15:02:02 +0000

    testing & minor fixes

--- RedBlack.hs
+++ RedBlack.hs
@@ -31,25 +31,24 @@
 module RedBlack where
 
 -- Definition of the Red-Black Tree data structure
-
 data Color = Red | Black
   deriving (Eq,Show)
 
 data RBT a = LeafRB | NodeRB Color (RBT a) a (RBT a)
   deriving (Eq,Show)
 
+
 -- Serching a key inside a red-black tree
 --   return True if the key is found, False otherwise
-
 searchRB :: Ord a => a -> RBT a -> Bool
 searchRB _ LeafRB = False
-searchRB a (NodeRB c l x r) = 
-  (a == x) || if (a < x) then searchRB a l
-                         else searchRB a r
+searchRB n (NodeRB c l x r) = 
+  (n == x) || if (n < x) then searchRB n l
+                         else searchRB n r
+
 
 -- Minimum and maximum of red-black tree
 --   return Nothing if the tree is empty
-
 minRB :: RBT a -> Maybe a
 minRB LeafRB = Nothing
 minRB (NodeRB c LeafRB x r) = Just x
@@ -64,29 +63,35 @@ maxRB (NodeRB c l x r) = maxRB r
 -- Check if a tree satisfies the Binary Search Tree condition
 --   (do not check other RBT conditions)
 isBST :: Ord a => RBT a -> Bool
-isBST t = boundBST Nothing t Nothing
+isBST t = boundCheck Nothing t Nothing
 
-boundBST :: Ord a => Maybe a -> RBT a -> Maybe a -> Bool
-boundBST lb LeafRB rb = lb `leqM` rb
-boundBST lb (NodeRB c l x r) rb = 
-  boundBST lb l (Just x) && boundBST (Just x) r rb
+boundCheck :: Ord a => Maybe a -> RBT a -> Maybe a -> Bool
+boundCheck l LeafRB r = l `checkSubtree` r
+boundCheck l2 (NodeRB c l x r) r2 = 
+  boundCheck l2 l (Just x) && boundCheck (Just x) r r2
 
-leqM :: Ord a => Maybe a -> Maybe a -> Bool
-leqM (Just x) (Just y) = x <= y
-leqM _ _ = True
+checkSubtree :: Ord a => Maybe a -> Maybe a -> Bool
+checkSubtree (Just x) (Just y) = x <= y
+checkSubtree _ _ = True
+
 
 -- Check the Black-balancing condition:
 --     all paths have the same number of black nodes
-
 blackBalanced :: RBT a -> Bool
 blackBalanced LeafRB = True
 blackBalanced (NodeRB c l x r) = 
   let diff = abs (countBlack l - countBlack r) in
-    diff == 0 && blackBalanced l && blackBalanced r
+      diff == 0 && blackBalanced l && blackBalanced r
+
+--Auxillary function to count the number of black nodes in each subtree
+countBlack :: RBT a -> Int
+countBlack LeafRB = 0
+countBlack (NodeRB c l x r) =
+  if (c == Black) then countBlack l + 1
+                  else countBlack l + 0
 
 
 -- Black height of a black-balanced tree, -1 if not black-balanced
-
 blackHeight :: RBT a -> Int
 blackHeight LeafRB = 0
 blackHeight (NodeRB c l x r) = 
@@ -94,33 +99,26 @@ blackHeight (NodeRB c l x r) =
                                       else -1
 
 
-countBlack :: RBT a -> Int
-countBlack LeafRB = 0
-countBlack (NodeRB c l x r) =
-  if (c == Black) then countBlack l + countBlack r + 1
-                  else countBlack l + countBlack r + 0
-
-
 -- Check if all Red-Black Tree conditions are satisfied
---MODIFY THIS, check if is BST and that it is black balanced
 isRBT :: Ord a => RBT a -> Bool
 isRBT (NodeRB c l x r) = 
-  --If the colour of the root is black then check the subtrees
-  if (c == Black) then checkNodes l && checkNodes r
-                  --Else it is not an RBT
-                  else False
+  --If the colour of the root is black, the tree is a BST and is black balanced, check the left and right subtrees for recurring red nodes
+  if (c == Black) && (isBST (NodeRB c l x r)) && (blackBalanced (NodeRB c l x r)) then checkNodes l && checkNodes r                                        
+                                                                                  else False
 
+--Auxillary function to check for consecutive red nodes
 checkNodes :: Ord a => RBT a -> Bool
 --If you reach a leaf then true
 checkNodes LeafRB = True
-checkNodes (NodeRB c l x r) =
+checkNodes (NodeRB c l x r) =  
   --If the colour is red and the children are black then check the subtrees, else false
   if (c == Red) then (if (isBlack l && isBlack r) then checkNodes l && checkNodes r else False)
                 --Else check the subtree
                 else checkNodes l && checkNodes r
 
+--Auxillary function to check if a node is black
 isBlack :: Ord a => RBT a -> Bool
---Leaves are black
+--Count leaves as black
 isBlack LeafRB = True
 isBlack (NodeRB c l x r) = 
   --If the colour is black return true
@@ -128,16 +126,7 @@ isBlack (NodeRB c l x r) =
                   else False
 
 
-
 -- Insert a new element in a RBT, preserving the RBT properties
---INSERTION INTO A BST
-insertBST :: Ord a => a -> RBT a -> RBT a
-insertBST n LeafRB = (NodeRB Black (LeafRB) n (LeafRB))
-insertBST n (NodeRB c l x r) =
-  if n <= x then NodeRB c (insertBST n l) x r
-            else NodeRB c l x (insertBST n r)
-
---INSERTION INTO AN RBT
 insertRB :: Ord a => a -> RBT a -> RBT a
 insertRB n x = blackRoot (ins n x)
 
@@ -145,15 +134,17 @@ ins :: Ord a => a -> RBT a -> RBT a
 ins n LeafRB = (NodeRB Red (LeafRB) n (LeafRB))
 ins n t@(NodeRB c l x r)
                | n < x = balance c (ins n l) x r
-               | n > x = balance c l x (ins x r)
+               | n > x = balance c l x (ins n r)
                | otherwise = t
 
+--Auxillary function to turn a node black
 blackRoot :: Ord a => RBT a -> RBT a
 blackRoot LeafRB = LeafRB
 blackRoot (NodeRB c l x r) =
   if c == Black then (NodeRB c l x r)
                 else (NodeRB Black l x r)
 
+--Auxillary function to turn a node red
 redRoot :: Ord a => RBT a -> RBT a
 redRoot LeafRB = LeafRB
 redRoot (NodeRB c l x r) =
@@ -217,19 +208,18 @@ balR (NodeRB Red (NodeRB Black l x r) x2 r2) x3 r3 =
 fuse :: Ord a => RBT a -> RBT a -> RBT a
 fuse LeafRB x = x
 fuse x LeafRB = x
---fuse LeafRB (NodeRB Red l x r) = NodeRB Red l x r
---fuse (NodeRB Red l x r) LeafRB = NodeRB Red l x r
 fuse (NodeRB Red l x r) (NodeRB Red l2 x2 r2) =
   let s = fuse r l2
   in case s of
-    (NodeRB Red s1 z s2) -> (NodeRB Red (NodeRB Red l x s1) z (NodeRB Red s2 x2 r2))
+    (NodeRB Red s1 y s2) -> (NodeRB Red (NodeRB Red l x s1) y (NodeRB Red s2 x2 r2))
     LeafRB -> (NodeRB Red l x (NodeRB Red s x2 r2))
 fuse (NodeRB Black l x r) (NodeRB Black l2 x2 r2) =
   let s = fuse r l2
   in case s of
-    (NodeRB Red s1 z s2) -> (NodeRB Red (NodeRB Black l x s1) z (NodeRB Black s2 x2 r))
-    (NodeRB Black s1 z s2) -> balL l x (NodeRB Black s x2 r2)
+    (NodeRB Red s1 y s2) -> (NodeRB Red (NodeRB Black l x s1) y (NodeRB Black s2 x2 r))
+    (NodeRB Black s1 y s2) -> balL l x (NodeRB Black s x2 r2)
 
+--Auxillary function to get the colour of a node
 color :: Ord a => RBT a -> Color
 color LeafRB = Black
 color (NodeRB c l x r) = c
